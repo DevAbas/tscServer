a9638c18871ba34bee16165f1065ad96b429bdf3
DevAbas <abas.memmedov.94@gmail.com>
2020-04-13 22:12:29 +0400

    Login router has been completed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['dsghjdgsdhj'] }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
